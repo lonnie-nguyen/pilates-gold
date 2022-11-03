e94095bc04a8488c8d7799166d097bd863fd3853
lon <lhl.nguyen@gmail.com>
2022-11-02 18:43:46 -0700

    update accordion

--- app/src/screens/WeeklyTips.tsx
+++ app/src/screens/WeeklyTips.tsx
@@ -6,31 +6,12 @@ import {db} from "../../firebaseConfig";
 import { getStorage, ref, getDownloadURL } from "firebase/storage"
 
 export default class WeeklyTips extends React.Component<any, any> {
-    constructor(props: { title: string; tips: Array<string>; mods: Array<string>; image: string }) {
+    constructor(props: { title: string; tips: Array<string>; mods: Array<string>; imageURL: string }) {
         super(props);
         this.state = {
             data: {},
             imageURLs: {},
-            weeks:[
-                {
-                    title: 'Week 1',
-                    tips: ['Some tips go here this is filler text. Testing testing testing.', 'Some more text for texting.'],
-                    mods: ['Filler text.'],
-                    image: 'https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-                },
-                {
-                    title: 'Week 2',
-                    tips: ['Here is another inspirational picture. And some more filler text.', 'And some more text for texting here.', 'And here.'],
-                    mods: ['Filler modification.', 'Another filler modification.'],
-                    image: 'https://images.unsplash.com/photo-1554290712-e640351074bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1072&q=80',
-                },
-                {
-                    title: 'Week 3',
-                    tips: [],
-                    mods: [],
-                    image: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzF8fGluc3BpcmF0aW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-                },
-            ]
+
         }
     }
 
@@ -76,6 +57,9 @@ export default class WeeklyTips extends React.Component<any, any> {
         return (
             <View style={styles.container}>
                 <ScrollView showsHorizontalScrollIndicator={false}
+                            alwaysBounceHorizontal={false}
+                            alwaysBounceVertical={false}
+                            bounces={false}
                             contentContainerStyle={{ paddingHorizontal: 10, paddingVertical: 10}}>
                     { this. renderAccordions() }
                 </ScrollView>
@@ -84,9 +68,10 @@ export default class WeeklyTips extends React.Component<any, any> {
     }
     renderAccordions = () => {
         const items = [];
-        for (let item of this.state.weeks) {
+        for (const [key, value] of Object.entries(this.state.data)) {
             items.push(
-                <Accordion title={item.title} tips={item.tips} mods={item.mods} image={item.image} />
+                <Accordion tips={this.state.data[key].tips} mods={this.state.data[key].mods}
+                           title={this.state.data[key].title} image={this.state.imageURLs[key]} />
             );
         }
         return items;
